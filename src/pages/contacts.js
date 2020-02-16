b744c443e780a36a8e7eec3b9eac6f15d02842cc
tikken <tikken23@gmail.com>
2020-02-16 21:31:43 +0300

    contacts page

---
+++ src/pages/contacts.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Contacts = () => (
+  <Layout>
+    <SEO title="Contacts" />
+
+
+  </Layout>
+)
+
+export default Contacts
