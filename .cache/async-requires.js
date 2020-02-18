ef6c268670ac1f16cbc91bd3dc02a3704ce1ef4f
tikken <tikken23@gmail.com>
2020-02-18 20:58:00 +0300

    build

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -7,13 +7,13 @@ exports.components = {
   "component---src-pages-404-js": () => import("./../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-about-js": () => import("./../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
   "component---src-pages-blog-js": () => import("./../src/pages/blog.js" /* webpackChunkName: "component---src-pages-blog-js" */),
+  "component---src-pages-cabinet-js": () => import("./../src/pages/cabinet.js" /* webpackChunkName: "component---src-pages-cabinet-js" */),
   "component---src-pages-catalog-js": () => import("./../src/pages/catalog.js" /* webpackChunkName: "component---src-pages-catalog-js" */),
   "component---src-pages-contacts-js": () => import("./../src/pages/contacts.js" /* webpackChunkName: "component---src-pages-contacts-js" */),
   "component---src-pages-hits-js": () => import("./../src/pages/hits.js" /* webpackChunkName: "component---src-pages-hits-js" */),
   "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-information-js": () => import("./../src/pages/information.js" /* webpackChunkName: "component---src-pages-information-js" */),
   "component---src-pages-sales-js": () => import("./../src/pages/sales.js" /* webpackChunkName: "component---src-pages-sales-js" */),
-  "component---src-pages-tracking-js": () => import("./../src/pages/tracking.js" /* webpackChunkName: "component---src-pages-tracking-js" */),
-  "component---src-pages-cabinet-js": () => import("./../src/pages/cabinet.js" /* webpackChunkName: "component---src-pages-cabinet-js" */)
+  "component---src-pages-tracking-js": () => import("./../src/pages/tracking.js" /* webpackChunkName: "component---src-pages-tracking-js" */)
 }
 
