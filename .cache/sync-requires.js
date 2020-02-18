29d8eb37de03feceb51719d1842f25bd9c8c00b0
tikken <tikken23@gmail.com>
2020-02-18 23:58:16 +0300

    new build

---
+++ .cache/sync-requires.js
@@ -0,0 +1,22 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---src-templates-product-js": hot(preferDefault(require("/Users/tikken/Sites/printers/site/src/templates/product.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/tikken/Sites/printers/site/src/pages/404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/tikken/Sites/printers/site/src/pages/about.js"))),
+  "component---src-pages-blog-js": hot(preferDefault(require("/Users/tikken/Sites/printers/site/src/pages/blog.js"))),
+  "component---src-pages-cabinet-js": hot(preferDefault(require("/Users/tikken/Sites/printers/site/src/pages/cabinet.js"))),
+  "component---src-pages-cart-js": hot(preferDefault(require("/Users/tikken/Sites/printers/site/src/pages/cart.js"))),
+  "component---src-pages-catalog-js": hot(preferDefault(require("/Users/tikken/Sites/printers/site/src/pages/catalog.js"))),
+  "component---src-pages-contacts-js": hot(preferDefault(require("/Users/tikken/Sites/printers/site/src/pages/contacts.js"))),
+  "component---src-pages-hits-js": hot(preferDefault(require("/Users/tikken/Sites/printers/site/src/pages/hits.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/tikken/Sites/printers/site/src/pages/index.js"))),
+  "component---src-pages-information-js": hot(preferDefault(require("/Users/tikken/Sites/printers/site/src/pages/information.js"))),
+  "component---src-pages-sales-js": hot(preferDefault(require("/Users/tikken/Sites/printers/site/src/pages/sales.js"))),
+  "component---src-pages-tracking-js": hot(preferDefault(require("/Users/tikken/Sites/printers/site/src/pages/tracking.js")))
+}
+
