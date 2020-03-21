0c10c0480a0fd86619f2a2725b36d469120c4f3b
tikken <tikken23@gmail.com>
2020-03-21 09:05:19 +0300

    fix

--- src/components/landing/BlockSlider.js
+++ src/components/landing/BlockSlider.js
@@ -1,8 +1,116 @@
-import React from 'react'
-import CustomizedSlider from './BlockSliderSlide'
+import React, { useEffect } from 'react'
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Slider from '@material-ui/core/Slider';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: "100%"
+    },
+    margin: {
+        height: theme.spacing(3),
+    },
+}));
+
+const marks = [];
+(function () {
+    for (let step = 1; step < 10; step++) {
+        marks.push({value: step});
+    }
+})();
+
+let words = ['год', 'года', 'лет'];
+
+function valuetext(value) {
+    let line;
+
+    if(value < 2) {
+        line = `${value} ${words[0]}`
+    }
+
+    if(value >= 2) {
+        line = `${value} ${words[1]}`
+    }
+
+    if(value >= 5) {
+        line = `${value} ${words[2]}`
+    }
+
+    return line;
+}
+
+const IOSSlider = withStyles({
+    root: {
+        color: '#40404C',
+        height: 2,
+        padding: '15px 0',
+        width: '100%'
+    },
+    thumb: {
+        height: 30,
+        width: 5,
+        backgroundColor: '#40404C',
+        marginRight: 5,
+        marginLeft: 0,
+        borderRadius: 0,
+        top: 11
+    },
+    valueLabel: {
+        left: -23,
+        top: -25,
+        width: 50,
+        '& *': {
+            background: 'transparent',
+            color: '#000',
+        },
+    },
+    track: {
+        height: 10
+    },
+    rail: {
+        height: 9,
+        opacity: 0.5,
+        backgroundColor: '#fff',
+        border: '1px solid black'
+    },
+    mark: {
+        backgroundColor: '#bfbfbf',
+        height: 10,
+        width: 1,
+        marginTop: 0,
+    },
+    markActive: {
+        opacity: 0,
+        backgroundColor: 'currentColor',
+    }
+})(Slider);
+
 
 export default function Block({image, lie, truth, desc, footer, data}) {
     const reversed = data.reverse();
+    const classes = useStyles();
+    const prices = [];
+
+    reversed.map((el) => prices.push(el.node.price));
+
+    useEffect(() => {
+
+        let block = document.querySelector('.land_block-slider');
+        let factor = parseFloat(block.querySelector('.MuiSlider-thumb').getAttribute('aria-valuenow'));
+
+        let elements = block.querySelectorAll('.land_block-item_slider-item_benefit-item_card-price-item_val');
+
+        let bad = parseFloat(elements[0].innerText) * factor;
+        let good = parseFloat(elements[1].innerText) * factor;
+
+        let mui = block.querySelector('.MuiSlider-root')
+
+        mui.addEventListener('click', () => {
+            prices[0] = bad;
+            prices[1] = good;
+
+            console.log(prices, 'damn damn')
+        });
+    });
 
     return (
         <div className="land">
@@ -38,7 +146,13 @@ export default function Block({image, lie, truth, desc, footer, data}) {
                                 <span className="land_block-item_slider-item_label-max">10 лет</span>
                             </div>
 
-                            <CustomizedSlider/>
+                            <div className={classes.root}>
+                                <IOSSlider
+                                    aria-label="ios slider"
+                                    valueLabelDisplay="on"
+                                    getAriaValueText={valuetext}
+                                    defaultValue={5} min={1} max={10}/>
+                            </div>
 
                         </div>
 
@@ -54,7 +168,9 @@ export default function Block({image, lie, truth, desc, footer, data}) {
                                                 </div>
                                                 <div className="land_block-item_slider-item_benefit-item_card-price">
                                                     <div className="land_block-item_slider-item_benefit-item_card-price-item">
-                                                        <span className="land_block-item_slider-item_benefit-item_card-price-item_val" data-price={el.node.price}>{el.node.price}</span>
+                                                        <span className="land_block-item_slider-item_benefit-item_card-price-item_val">
+                                                            {el.node.price}
+                                                        </span>
                                                         <img src={'/icons/rub.svg'} alt=""/>
                                                     </div>
                                                 </div>
